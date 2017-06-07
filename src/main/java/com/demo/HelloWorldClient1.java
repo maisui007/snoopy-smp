bfbd515a6155855cb1c03601aa4cdb5b0b250c11
maisui007 <zhangjnmg@163.com>
2017-06-07 20:31:02 +0800

    结合成spring

---
+++ src/main/java/com/demo/HelloWorldClient1.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+
+
+public class HelloWorldClient1 {
+    public static void main(String[] args) {
+        JaxWsProxyFactoryBean svr = new JaxWsProxyFactoryBean();
+        svr.setServiceClass(HelloWorld.class);
+        svr.setAddress("http://localhost:8080/helloWorld");
+        HelloWorld hw = (HelloWorld) svr.create();
+        User user = new User();
+        user.setName("Tony");
+        user.setDescription("test");
+        System.out.println(hw.sayHiToUser(user));
+    }
+}
